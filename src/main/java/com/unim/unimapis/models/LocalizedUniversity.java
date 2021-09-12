966051daf566760360a9351d5f2934a036a30c07
rashadnagiev <rashad.naghiyev-external@sema4genomics.com>
2021-09-13 01:29:56 +0400

    mini fix

--- src/main/java/com/unim/unimapis/models/LocalizedUniversity.java
+++ src/main/java/com/unim/unimapis/models/LocalizedUniversity.java
@@ -1,19 +1,18 @@
 package com.unim.unimapis.models;
 
 import com.unim.unimapis.models.localization.LocalizedId;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.experimental.FieldDefaults;
-import org.hibernate.annotations.Cache;
-import org.hibernate.annotations.CacheConcurrencyStrategy;
-
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.MapsId;
 import javax.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
 
 @Getter
 @Setter
